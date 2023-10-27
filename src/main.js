4db20a8e8a5efe8fd6d2cf7e717ba298a8a6ddb8
Artem Selifonov <selifonov.artem@mail.ru>
2023-10-27 16:28:03 +0300

    Renamed names of components to correct ones. Moved router configuration to router.js.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,6 @@
-import MainPage from "@/components/MainPage.vue";
-import RouteListPage from "@/components/RouteListPage.vue";
-import RoutePage from "@/components/RoutePage.vue";
 import {createApp} from "vue";
 import App from "./App.vue";
-import {createRouter, createWebHashHistory} from "vue-router";
-
-
-const routes = [
-  {path: "/", component: MainPage},
-  {path: "/routes", component: RouteListPage},
-  {path: "/routes/:id", component: RoutePage},
-];
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+import {router} from "./router";
 
 const app = createApp(App);
 
