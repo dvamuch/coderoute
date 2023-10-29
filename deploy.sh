cdf3fe0d91c6c77abf741f3de0430c5f2da37439
Putselovsky Evgeniy <E9315717@yandex.ru>
2023-10-29 10:43:19 +0300

    Added configuration of dist file in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,7 @@ ssh -p 2425 root@10.0.2.15 /bin/bash <<EOF
   git add .
   git commit -m "Local changes"
   git pull origin master
+  npm run build
+  cp -r dist/* /usr/share/nginx/html/domain/
   exit
 EOF
