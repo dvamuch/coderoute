f4ef3b06c0c6aee75b9cf59a154b2831bb8a05ef
Putselovsky Evgeniy <E9315717@yandex.ru>
2023-11-03 19:00:37 +0300

    Removed some config in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ssh -p 2425 root@10.0.2.15 /bin/bash <<EOF 
-  sudo echo "New value"
+  #sudo echo "New value"
   cd fronted_project
   cd coderoute
   git add .
   git commit -m "Local changes"
   git pull origin master
-  npm run build
-  cp -r dist/* /usr/share/nginx/html/domain/
+ # npm run build
+#  cp -r dist/* /usr/share/nginx/html/domain/
   exit
 EOF
