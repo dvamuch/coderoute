4dd814f7de3d450ebbe2d2a6985547c5f7102631
Putselovsky Evgeniy <E9315717@yandex.ru>
2023-11-16 23:06:29 +0300

    Changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
 
 ssh -p 2425 root@10.0.2.15 /bin/bash <<EOF 
-<<<<<<< HEAD
   #sudo echo "New value"
   cd fronted_project
-=======
-<<<<<<< HEAD
-  #sudo echo "New value"
-  cd fronted_project
-=======
-  sudo echo "New value"
-  cd frontend_projectv2
->>>>>>> af15133220a6cde7e022226a0a2eace6e39f3148
->>>>>>> master
-  cd coderoute
   git add .
   git commit -m "Local changes"
   git pull origin master
-<<<<<<< HEAD
   # npm run build
   # cp -r dist/* /usr/share/nginx/html/domain/
-=======
- # npm run build
-#  cp -r dist/* /usr/share/nginx/html/domain/
->>>>>>> master
   exit
 EOF
