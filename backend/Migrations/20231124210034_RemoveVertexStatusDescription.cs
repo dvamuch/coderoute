ef06d6b83be1969fcd9f01b1da09f6421ef405ca
ALEX <alexsmi0807@mail.ru>
2023-11-25 00:15:49 +0300

    Fix appsetting.json file. Add migrations.

---
+++ backend/Migrations/20231124210034_RemoveVertexStatusDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeRoute.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveVertexStatusDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusDescription",
+                table: "VertexStatuses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StatusDescription",
+                table: "VertexStatuses",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
