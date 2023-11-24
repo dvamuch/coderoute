17be6af4644cabee53d5118ae10c152c582ba84d
ALEX <alexsmi0807@mail.ru>
2023-11-24 20:09:18 +0300

    Return statusId instead of status value in Route

--- backend/DTO/Node.cs
+++ backend/DTO/Node.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Status { get; set; }
+        public int StatusId { get; set; }
         public List<Node>  SecondatyNode { get; set; }
     }
 }
