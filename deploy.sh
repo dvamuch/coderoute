118ad21fa2645546cbfe3519086b45a5d860b27f
Glawary <105108436+Glawary@users.noreply.github.com>
2023-12-03 00:51:37 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 ssh evgeny@10.128.0.31 /bin/bash <<EOF
   cd project
   cd coderoute
+  git add .
+  git commit -m "Local changes"
   git pull origin dev
   docker compose -f docker-compose-production.yaml up -d --no-deps --build backend
   docker compose -f docker-compose-production.yaml up -d --no-deps --build frontend
