2f3792b9c76a1acb13b484e49dca4c854cc8fc8f
Artem Selifonov <selifonov.artem@mail.ru>
2023-12-08 16:00:13 +0300

    Добавил проверку на мобилку. Пока просто надпись

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import clickOutDirective from "@/directives/clickOut";
 import {createPinia} from "pinia";
 import {createApp} from "vue";
+import VueMobileDetection from "vue-mobile-detection";
 import App from "./App.vue";
 import router from "./router";
 
@@ -9,6 +10,7 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(VueMobileDetection);
 app.directive("click-out", clickOutDirective);
 app.mount("#app");
 
