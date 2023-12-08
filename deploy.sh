5d5594d53de20c019a35f39d5580337734927d3c
Glawary <105108436+Glawary@users.noreply.github.com>
2023-12-08 21:05:24 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ ssh evgeny@10.128.0.31 /bin/bash <<EOF
   git add .
   git commit -m "Local changes"
   git pull origin dev
-  docker compose -f docker-compose-production.yaml up -d --no-deps --build backend
-  docker compose -f docker-compose-production.yaml up -d --no-deps --build frontend
   
   exit
 EOF
